aa2677756d6cf3389ef458dd70dad55ada9d49be
Sergio Obalski Filho <serginhobalski@gmail.com>
2021-09-25 14:20:56 -0300

    ajustes e customização

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*var nome = prompt("Digite o seu nome:");
+alert("Seja bem-vindo(a), " + nome + ", ao meu site!");
+*/
+
+alert("Seja bem-vindo(a)!");
+
+function clicou(){
+    alert("Obligado por testar!");
+}
+
